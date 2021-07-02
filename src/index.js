8d8aa1c492935cc28ba5360a19734ff3641ba7dd
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-07-02 08:11:20 +0300

    feat: Add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array !== "") {
+        let min = Math.min.apply(null,array);
+        console.log(min);
+        return min;
+    } else {
+        return 0
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array !== "") {
+        let max = Math.max.apply(null,array);
+        console.log(max);
+        return max;
+    } else {
+        return 0
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array !== "") {
+        const avg = (acc, current) => acc + current;
+        let res = (array.reduce(avg))/array.length;
+        console.log(res);
+        return res;
+    } else {
+        return 0
+    }
 }
